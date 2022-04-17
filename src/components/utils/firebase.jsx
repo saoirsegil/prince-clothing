dbb2008dd1f4970b63100b5ba9b400c43b5f9ca1
Jayson Alihassan Diaz <95516878+jaysonprincediaz@users.noreply.github.com>
2022-04-17 10:37:19 +0800

    created authentication page component alongside sign in form

--- src/components/utils/firebase.jsx
+++ src/components/utils/firebase.jsx
@@ -5,7 +5,8 @@ import {
     signInWithRedirect,
     signInWithPopup,
     GoogleAuthProvider,
-    createUserWithEmailAndPassword
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword
  } from "firebase/auth"
  import {
   getFirestore,
@@ -64,4 +65,10 @@ return userDocRef;
 export const createAuthUserWithEmailAndPassword = async (email, password) => {
   if (!email || !password) return;
   return await createUserWithEmailAndPassword(auth, email, password)
+}
+
+
+export const signInAuthUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) return;
+  return await signInWithEmailAndPassword(auth, email, password)
 }
---
+++ src/routes/authentication/Authentication.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import SignUpForm from '../../components/sign-up-form/SignUpForm'
+import SignInForm from '../../components/sign-in-form/SignInForm'
+import './Authentication.scss'
+
+const Authentication = () => {
+    return <div className='authentication-container'>
+        <SignInForm/>
+        <SignUpForm/>
+    </div>
+}
+
+export default Authentication
